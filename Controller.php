b7f6eb3094f84dae3b964f97ea616b6ab8e6765b
Domesticbadger <hook_hood@hotmail.com>
2019-11-26 13:08:32 -0700

    commit

--- Controller.php
+++ Controller.php
@@ -1,1 +1,13 @@
 <?php
+$game_store = new DatabaseAdapter();
+if(isset( $_GET ['command])&& $_GET['command'] === "home"){
+}
+if(isset( $_GET ['user'])&& isset($_GET['pass'])){
+	$user = $_GET['user']
+	$pass = $_GET['pass']
+	$array = $game_store->tryLogin($user,$pass)
+	if (count($array) === 0){
+		echo json_encode(["No User Found"])
+	}
+	else{
+		echo json_encode(
